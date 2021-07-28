d9d0ee4d007aec683dad245a70bbc9b96ec40e8e
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-07-28 13:39:51 -0400

    initial material ui setup This commit adds material ui to app
    These changes are needed because mvp

--- template/pages/_app.tsx
+++ template/pages/_app.tsx
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import React from 'react';
+import Head from 'next/head';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    
+    let jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      // @ts-ignore: Object is possibly 'null'.
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  // Set dark/light mode based on user preference
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My App</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 export default MyApp
--- template/pages/index.tsx
+++ template/pages/index.tsx
@@ -1,1 +1,7 @@
+import * as React from 'react'
 
+const Home = () => {
+
+}
+
+export default Home
