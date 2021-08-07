1da87efd1f8f13c4437943a7c000fd9b87816439
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-08-07 10:28:31 -0400

    refactor and add tests for basicValidateEmailSubmission

--- template/src/template/prebuiltUI/forms/validationUtils.ts
+++ template/src/template/prebuiltUI/forms/validationUtils.ts
@@ -1,18 +1,22 @@
 /**
  * @description Simple email form validation that checks user input before submission.
  *
- * @param email
+ * @param email email to be validated
+ * @param emailRequiredMessage error message if a email was not provided
+ * @param invalidMessage error message if email is invalid format
  * @returns undefined if no error or an error message string
  */
 export const basicValidateEmailSubmission = (
-  email: string
+  email: string,
+  emailRequiredMessage?: string,
+  invalidMessage?: string
 ): string | undefined => {
   let error;
 
   if (!email) {
-    error = "Email is required.";
+    error = emailRequiredMessage ? emailRequiredMessage : "Email is required.";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
-    error = "Invalid email address";
+    error = invalidMessage ? invalidMessage : "Invalid email address.";
   }
 
   return error;
