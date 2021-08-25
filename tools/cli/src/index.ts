8eade31ede6a7e179cb619b1ca5bebe121712cbd
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-08-24 21:39:51 -0400

    begin implementation for stripe checkout subscriptions use command

--- tools/cli/src/index.ts
+++ tools/cli/src/index.ts
@@ -10,9 +10,17 @@ program.version("0.0.1");
 program
   .command("init")
   .description("Initialize a new hyper-next Next.js project.")
-  .option("-n, --name [value]", "Application Name")
+  .option("-n, --name [text]", "Application Name")
   .action((args) => {
     init(args);
   });
+
+program
+  .command("use")
+  .description("Use a hyper-next package in a hyper-next project.")
+  .requiredOption(
+    "-p, --package [package name]",
+    "Package name. See https://hyper-next.com/docs/cli/use#packages for list of available package names."
+  );
 
 program.parse(process.argv);
