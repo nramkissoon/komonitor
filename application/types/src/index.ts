7b45a88cdaaf09ff27ce03b0a017dd0b68c1c083
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-09-09 13:41:22 -0400

    add user

--- application/types/src/index.ts
+++ application/types/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./config";
 export * from "./job-runner";
 export * from "./uptime-check";
+export * from "./user";
---
+++ application/types/src/user/index.ts
@@ -0,0 +1,25 @@
+// DynamoDB model for a user
+
+export interface NextAuthUserAttributes {
+  pk: string;
+  sk: string;
+  id: string;
+  type: "USER";
+  name: string | undefined;
+  email: string | undefined;
+  image: string | undefined;
+  username: unknown;
+  emailVerified: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface StripeAttributes {
+  customer_id?: string;
+  subscription_id?: string;
+  current_period_end?: string;
+  subscription_status?: string;
+  product_id?: string;
+}
+
+export interface User extends NextAuthUserAttributes, StripeAttributes {}
