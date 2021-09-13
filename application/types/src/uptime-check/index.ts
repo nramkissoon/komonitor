5c593817dae5b602c1a81be2f68d5ee8a1fa4dad
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-09-13 12:09:58 -0400

    add core type

--- application/types/src/uptime-check/index.ts
+++ application/types/src/uptime-check/index.ts
@@ -4,29 +4,24 @@ import {
   UptimeCheckSupportedFrequenciesInMinutes,
 } from "../config/index";
 
-export interface UptimeMonitorWebhookNotification {
+export interface CoreUptimeMonitor {
   url: string;
   name: string;
-  trigger: "up" | "down";
   region: SupportedRegion;
-  monitor_type: MonitorType;
-  latency: number;
-}
-
-export interface UptimeMonitor {
-  owner_id: string;
-  monitor_id: string;
-  created_at: number;
-  last_updated: number;
-  url: string;
-  name: string;
-  regions: SupportedRegion[];
   frequency: UptimeCheckSupportedFrequenciesInMinutes;
   retries: number;
   failures_before_alert?: number;
   webhook_url?: string;
   alert_ids?: string[];
 }
+
+export interface UptimeMonitor extends CoreUptimeMonitor {
+  owner_id: string;
+  monitor_id: string;
+  created_at: number;
+  last_updated: number;
+}
+
 export interface UptimeMonitorJob {
   monitor_id: string;
   url: string;
@@ -42,4 +37,13 @@ export interface UptimeMonitorStatus {
   status: "up" | "down";
   latency: number;
   region: SupportedRegion;
+}
+
+export interface UptimeMonitorWebhookNotification {
+  url: string;
+  name: string;
+  trigger: "up" | "down";
+  region: SupportedRegion;
+  monitor_type: MonitorType;
+  latency: number;
 }
