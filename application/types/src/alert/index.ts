6885f5027d942ea3c558147827315dd5191fd90a
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-09-14 12:51:18 -0400

    add alert types

---
+++ application/types/src/alert/index.ts
@@ -0,0 +1,30 @@
+export enum AlertTypes {
+  Email = "Email",
+  Slack = "Slack",
+}
+
+export enum AlertSeverities {
+  Warning = "Warning",
+  Severe = "Severe",
+  Critical = "Critical",
+}
+
+export enum AlertStatuses {
+  On = "on",
+  Paused = "paused",
+}
+
+export interface EditableAlertAttributes {
+  name: string;
+  description: string;
+  severity: AlertSeverities;
+  recipients: string[];
+  status: AlertStatuses;
+}
+
+export interface Alert extends EditableAlertAttributes {
+  owner_id: string;
+  alert_id: string;
+  created_at: number;
+  last_updated: number;
+}
