75c0eea69db6fc0df56a18dd1fbcebdeef17a9e1
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-09-17 12:46:25 -0400

    basic styling for description cell

--- application/webapp/src/modules/uptime/components/Overview-Table-Cell.tsx
+++ application/webapp/src/modules/uptime/components/Overview-Table-Cell.tsx
@@ -1,5 +1,6 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Text, useColorModeValue } from "@chakra-ui/react";
 import Link from "next/link";
+import React from "react";
 
 interface DescriptionCellProps {
   monitorId: string;
@@ -9,11 +10,26 @@ interface DescriptionCellProps {
 
 export function DescriptionCell(props: DescriptionCellProps) {
   return (
-    <>
+    <Box>
       <Link passHref href={`/app/uptime/${props.monitorId}`}>
-        {props.name}
+        <Box
+          _hover={{
+            cursor: "pointer",
+            color: useColorModeValue("purple.600", "purple.300"),
+          }}
+          w="max-content"
+        >
+          <Text
+            mb=".6em"
+            fontSize="xl"
+            fontWeight="normal"
+            letterSpacing="normal"
+          >
+            {props.name}
+          </Text>
+          <Text fontWeight="thin">{props.url}</Text>
+        </Box>
       </Link>
-      <Text>{props.url}</Text>
-    </>
+    </Box>
   );
 }
