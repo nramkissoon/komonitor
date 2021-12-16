b25d40ad7325d32784bd3960e51ca3f4a3ec4bfb
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-12-16 13:57:13 -0500

    drop mdx prov

--- application/webapp/src/modules/docs/components/Mdx.tsx
+++ application/webapp/src/modules/docs/components/Mdx.tsx
@@ -1,7 +1,4 @@
-import * as chakraComponents from "@chakra-ui/react";
-import { MDXProvider } from "@mdx-js/react";
 import * as React from "react";
-import { MDXComponents } from "../../../common/components/Mdx-Components";
 import { getRouteContext } from "../../mdx-utils/get-route-context";
 import { SidebarRoutes } from "../content";
 import {
@@ -11,14 +8,6 @@ import {
 import PageContainer from "./Page-Container";
 import Pagination from "./Paginations";
 import Sidebar, { RouteItem } from "./Sidebar";
-
-export function MDXLayoutProvider({ children }: any) {
-  return (
-    <MDXProvider components={{ ...chakraComponents, ...MDXComponents } as any}>
-      {children}
-    </MDXProvider>
-  );
-}
 
 interface MDXLayoutProps {
   frontmatter: any;
@@ -33,19 +22,17 @@ export default function MDXLayout(props: MDXLayoutProps) {
   const routeContext = getRouteContext(route as RouteItem, routes);
 
   return (
-    <MDXLayoutProvider>
-      <PageContainer
-        frontmatter={frontmatter}
-        sidebar={<Sidebar routes={routes} />}
-        pagination={
-          <Pagination
-            next={routeContext.nextRoute}
-            previous={routeContext.prevRoute}
-          />
-        }
-      >
-        {children}
-      </PageContainer>
-    </MDXLayoutProvider>
+    <PageContainer
+      frontmatter={frontmatter}
+      sidebar={<Sidebar routes={routes} />}
+      pagination={
+        <Pagination
+          next={routeContext.nextRoute}
+          previous={routeContext.prevRoute}
+        />
+      }
+    >
+      {children}
+    </PageContainer>
   );
 }
