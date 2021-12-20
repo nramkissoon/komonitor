69d48e1614ec85644a967ba342b104e8cfb43e60
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-12-19 22:19:01 -0500

    remove slide fades ew

--- application/webapp/src/modules/landing-page/Banner.tsx
+++ application/webapp/src/modules/landing-page/Banner.tsx
@@ -6,7 +6,6 @@ import {
   HStack,
   Icon,
   Image,
-  SlideFade,
   useColorModeValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
@@ -47,65 +46,59 @@ export function Banner() {
   return (
     <Flex mb="5em" flexDir="column" alignItems="center">
       <Box>
-        <SlideFade in offsetY="40px" delay={0.2}>
-          <chakra.h1
-            textAlign="center"
-            fontSize="7xl"
-            fontWeight="extrabold"
-            color={useColorModeValue("gray.800", "gray.100")}
-            lineHeight="shorter"
-            mb=".3em"
-          >
-            {Copy.MainBanner.Header}
-          </chakra.h1>
-        </SlideFade>
-        <SlideFade in offsetY="40px" delay={0.3}>
-          <chakra.h2
-            fontSize="3xl"
-            fontWeight="bold"
-            textAlign="center"
-            lineHeight="shorter"
-            w={["70%"]}
-            mx="auto"
-            color={useColorModeValue("gray.600", "gray.400")}
-            mb=".8em"
-          >
-            {Copy.MainBanner.Subheader}
-          </chakra.h2>
-        </SlideFade>
-      </Box>
-      <SlideFade in offsetY="40px" delay={0.5}>
-        {CtaButton()}
-      </SlideFade>
-      <SlideFade in offsetY="40px" delay={0.5}>
-        <chakra.h3 color="gray.500" textAlign="center" mb="2em">
-          {Copy.MainBanner.CtaButtonHelperText}
-        </chakra.h3>
-      </SlideFade>
-      <SlideFade in offsetY="40px" delay={0.5}>
-        <Box
-          bg={useColorModeValue("gray.700", "gray.600")}
-          borderRadius="2xl"
-          shadow={useColorModeValue(["dark-lg"], ["dark-lg"])}
-          px={["1em", null, "1.5em"]}
-          pt="5px"
-          pb={["1em", null, "1.5em"]}
+        <chakra.h1
+          textAlign="center"
+          fontSize="7xl"
+          fontWeight="extrabold"
+          color={useColorModeValue("gray.800", "gray.100")}
+          lineHeight="shorter"
+          mb=".3em"
         >
-          <HStack mb="5px">
-            {CircleIcon("purple.400")} {CircleIcon("yellow.400")}{" "}
-            {CircleIcon("red.400")}
-          </HStack>
-          <Image
-            borderRadius="lg"
-            src={"/app-dashboard2.jpg"}
-            width="1000px"
-            css={{
-              imageRendering: "-webkit-optimize-contrast",
-            }}
-            alt="Komonitor App Dashboard"
-          />
-        </Box>
-      </SlideFade>
+          {Copy.MainBanner.Header}
+        </chakra.h1>
+
+        <chakra.h2
+          fontSize="3xl"
+          fontWeight="bold"
+          textAlign="center"
+          lineHeight="shorter"
+          w={["70%"]}
+          mx="auto"
+          color={useColorModeValue("gray.600", "gray.400")}
+          mb=".8em"
+        >
+          {Copy.MainBanner.Subheader}
+        </chakra.h2>
+      </Box>
+
+      {CtaButton()}
+
+      <chakra.h3 color="gray.500" textAlign="center" mb="2em">
+        {Copy.MainBanner.CtaButtonHelperText}
+      </chakra.h3>
+
+      <Box
+        bg={useColorModeValue("gray.700", "gray.600")}
+        borderRadius="2xl"
+        shadow={useColorModeValue(["dark-lg"], ["dark-lg"])}
+        px={["1em", null, "1.5em"]}
+        pt="5px"
+        pb={["1em", null, "1.5em"]}
+      >
+        <HStack mb="5px">
+          {CircleIcon("purple.400")} {CircleIcon("yellow.400")}{" "}
+          {CircleIcon("red.400")}
+        </HStack>
+        <Image
+          borderRadius="lg"
+          src={"/app-dashboard2.jpg"}
+          width="1000px"
+          css={{
+            imageRendering: "-webkit-optimize-contrast",
+          }}
+          alt="Komonitor App Dashboard"
+        />
+      </Box>
     </Flex>
   );
 }
