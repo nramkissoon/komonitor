a81cc5d288a6c56fab5dac3e35d3af2928a85073
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2021-12-22 21:42:06 -0500

    last commit

--- application/webapp/src/modules/app-index-page/App-Index-Page.tsx
+++ application/webapp/src/modules/app-index-page/App-Index-Page.tsx
@@ -3,9 +3,11 @@ import {
   chakra,
   Flex,
   SimpleGrid,
+  Spacer,
   useColorModeValue,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { use24HourMonitorStatuses, useUptimeMonitors } from "../uptime/client";
 
 const Link = (props: { href: string; children: React.ReactNode }) => (
   <NextLink href={props.href} passHref>
@@ -17,6 +19,113 @@ const Link = (props: { href: string; children: React.ReactNode }) => (
     </chakra.a>
   </NextLink>
 );
+
+const UptimeMonitorPanel = () => {
+  const { monitors, isLoading, isError } = useUptimeMonitors();
+  const {
+    statuses,
+    isLoading: statusesIsLoading,
+    isError: statusesIsError,
+  } = use24HourMonitorStatuses(
+    monitors ? monitors.map((monitor) => monitor.monitor_id) : []
+  );
+
+  const downMonitorsTotal =
+    monitors && statuses
+      ? (
+          monitors.map((monitor) => {
+            const mostRecentStatus = statuses[monitor.monitor_id].sort(
+              (prev, next) => prev.timestamp - next.timestamp
+            )[0];
+
+            const isDown = mostRecentStatus
+              ? mostRecentStatus.status === "down"
+              : false;
+            if (isDown) return 1;
+            return 0;
+          }) as number[]
+        ).reduce((prev, next) => prev + next)
+      : 0;
+
+  console.log(downMonitorsTotal);
+
+  const totalMonitors = monitors ? monitors.length : 0;
+
+  return (
+    <Flex
+      bg={useColorModeValue("white", "#0f131a")}
+      shadow="lg"
+      borderRadius="md"
+      py="2em"
+      px="1.5em"
+      flexDir="column"
+    >
+      <chakra.h2
+        fontSize="xl"
+        fontWeight="bold"
+        textAlign={["center", "center", "left"]}
+      >
+        Uptime Monitors
+      </chakra.h2>
+      <chakra.h3 color="gray.600">
+        Create, manage and inspect uptime monitors for your websites.
+      </chakra.h3>
+      <chakra.hr my="10px" />
+      {monitors && !isLoading && totalMonitors === 0 ? (
+        <Flex flexDir="row" justifyContent="space-between" alignItems="center">
+          <chakra.h4 fontSize="lg">No monitors have been created.</chakra.h4>
+          <NextLink href="app/uptime/new" passHref>
+            <chakra.a
+              fontWeight="medium"
+              color={useColorModeValue("blue.400", "blue.500")}
+              _hover={{ cursor: "pointer", color: "gray.500" }}
+            >
+              + Create a Monitor
+            </chakra.a>
+          </NextLink>
+        </Flex>
+      ) : (
+        <Flex flexDir="row" justifyContent="space-between" alignItems="center">
+          <chakra.h4 fontSize="lg">Total monitors: {totalMonitors}</chakra.h4>
+          <NextLink href="app/uptime/new" passHref>
+            <chakra.a
+              fontWeight="medium"
+              color={useColorModeValue("blue.400", "blue.500")}
+              _hover={{ cursor: "pointer", color: "gray.500" }}
+            >
+              + Create a Monitor
+            </chakra.a>
+          </NextLink>
+        </Flex>
+      )}
+      <Flex flexDir="row" justifyContent="space-between" alignItems="center">
+        <chakra.h4 fontSize="lg">
+          Total current down monitors: {downMonitorsTotal}
+        </chakra.h4>
+      </Flex>
+      <Spacer />
+      <NextLink passHref href={"/app/uptime"}>
+        <Button
+          float="left"
+          size="lg"
+          as="a"
+          fontSize="xl"
+          fontWeight="normal"
+          px="1em"
+          shadow="md"
+          colorScheme="blue"
+          bgColor="blue.400"
+          color="white"
+          _hover={{
+            bg: "blue.600",
+          }}
+        >
+          View Monitors
+        </Button>
+      </NextLink>
+    </Flex>
+  );
+};
 
 const Panel = (props: {
   type: string;
@@ -72,30 +181,23 @@ export function AppIndexPage() {
   return (
     <Flex flexDir="column">
       <Flex
-        mb="2em"
+        mb="1em"
         justifyContent="space-between"
         flexDir={["column", "column", "row"]}
       >
-        <chakra.h3 fontSize="2xl" fontWeight="bold">
-          Welcome!
-        </chakra.h3>
-        <chakra.h3 fontSize="2xl" fontWeight="bold">
+        <chakra.h2 fontSize="xl" fontWeight="medium">
+          At a glance:
+        </chakra.h2>
+        <chakra.h2 fontSize="xl" fontWeight="medium">
           New to Komonitor? Check out the{" "}
           <Link href="/docs/getting-started/introduction">
             docs to get started
           </Link>
           !
-        </chakra.h3>
+        </chakra.h2>
       </Flex>
       <SimpleGrid columns={[1, 1, 2]} spacingX={[6, 10]} spacingY={[10]}>
-        <Panel
-          type={"✅ Uptime Monitors"}
-          description={
-            "Create, manage and inspect uptime monitors for your websites."
-          }
-          href={"/app/uptime"}
-          buttonText={"Go to Monitors"}
-        />
+        <UptimeMonitorPanel />
         <Panel
           type={"🚨 Alerts"}
           description={
