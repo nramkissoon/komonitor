92884cecab72921d4ba2e3dc2a52b65c025d47f4
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-01-03 13:27:03 -0500

    add slack alert lambda

---
+++ application/alert-lambda/src/slack-alert-handler.ts
@@ -0,0 +1,70 @@
+import { Alert, UptimeMonitor, User } from "project-types";
+import { regionToLocationStringMap } from "./config";
+
+const createUptimeMonitorSlackAlertMessage = (
+  alert: Alert,
+  monitor: UptimeMonitor
+) => {
+  return {
+    text: `${alert.severity} ALERT - ${monitor.name} Uptime Monitor  in ${
+      regionToLocationStringMap[monitor.region]
+    }`,
+    attachments: [
+      {
+        color: "#E53E3E",
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `*${monitor.url}* *DOWN* for ${monitor.failures_before_alert} uptime check(s).\n*${monitor.failures_before_alert} * ${monitor.frequency} of downtime detected.*`,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `*<https://komonitor.com/app/uptime/${monitor.monitor_id}|View monitor>*`,
+            },
+          },
+        ],
+      },
+    ],
+  };
+};
+
+export const sendUptimeMonitorSlackAlert = async (
+  monitor: UptimeMonitor,
+  alert: Alert,
+  user: User
+): Promise<boolean> => {
+  try {
+    if (!user.slack_installations || user.slack_installations.length === 0) {
+      throw new Error(`no slack installation for user ${user.id}`);
+    }
+
+    const webhook = user.slack_installations[0].incomingWebhook?.url;
+    if (!webhook) {
+      throw new Error(`no Slack webhook for user ${user.id}`);
+    }
+
+    const response = await fetch(webhook, {
+      method: "POST",
+      headers: new Headers({ "content-type": "application/json" }),
+      body: JSON.stringify(
+        createUptimeMonitorSlackAlertMessage(alert, monitor)
+      ),
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `failed to send alert for ${user.id} ${monitor.monitor_id}`
+      );
+    }
+
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
