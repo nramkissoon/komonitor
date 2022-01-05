940e016fd334440d1494beca5a3e02a2c124544b
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-01-05 15:22:35 -0500

    add alert name to slack message

--- application/alert-lambda/src/slack-alert-handler.ts
+++ application/alert-lambda/src/slack-alert-handler.ts
@@ -18,7 +18,7 @@ const createUptimeMonitorSlackAlertMessage = (
             type: "section",
             text: {
               type: "mrkdwn",
-              text: `*${monitor.url}* *DOWN* for ${monitor.failures_before_alert} uptime check(s).\n*${monitor.failures_before_alert} X ${monitor.frequency} minutes of downtime detected.*\nAlert description: ${alert.description}`,
+              text: `*${monitor.url}* *DOWN* for ${monitor.failures_before_alert} uptime check(s).\n*${monitor.failures_before_alert} X ${monitor.frequency} minutes of downtime detected.*\nAlert name: ${alert.name}\nAlert description: ${alert.description}`,
             },
           },
           {
