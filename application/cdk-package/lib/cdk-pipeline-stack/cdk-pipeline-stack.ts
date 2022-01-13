07920ddea09d988aa186f5d19621fcc69a0a5514
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-01-13 15:44:17 -0500

    add build stage

--- application/cdk-package/lib/cdk-pipeline-stack/cdk-pipeline-stack.ts
+++ application/cdk-package/lib/cdk-pipeline-stack/cdk-pipeline-stack.ts
@@ -96,6 +96,7 @@ export class CdkPipelineStack extends cdk.Stack {
       lambdaCodeBuildActions.manualApprovalAction,
       lambdaCodeBuildActions.uptimeCheckCodeBuildAction,
       lambdaCodeBuildActions.jobRunnerCodeBuildAction,
+      lambdaCodeBuildActions.weeklyReportCodeBuildAction,
       lambdaCodeBuildActions.alertCodeBuildAction
     );
 
@@ -114,6 +115,7 @@ export class CdkPipelineStack extends cdk.Stack {
           `aws lambda update-function-code --function-name ${DEV_STACK.UPTIME_CHECK_LAMBDA} --s3-bucket ${LAMBDA_CODE_DEV_BUCKET} --s3-key ${UPTIME_CHECK_LAMBDA_CODE_KEY}`,
           `aws lambda update-function-code --function-name ${DEV_STACK.JOB_RUNNER_LAMBDA} --s3-bucket ${LAMBDA_CODE_DEV_BUCKET} --s3-key ${JOB_RUNNER_LAMBDA_CODE_KEY}`,
           `aws lambda update-function-code --function-name ${DEV_STACK.ALERT_LAMBDA} --s3-bucket ${LAMBDA_CODE_DEV_BUCKET} --s3-key ${ALERT_LAMBDA_CODE_KEY}`,
+          `aws lambda update-function-code --function-name ${DEV_STACK.WEEKLY_REPORT_LAMBDA} --s3-bucket ${LAMBDA_CODE_DEV_BUCKET} --s3-key ${WEEKLY_REPORT_LAMBDA_CODE_KEY}`,
         ],
         additionalArtifacts: [sourceArtifact],
         rolePolicyStatements: [
