eaefd33e3e470099ea0f4b10b4a6ae6a0c38bbd7
Nicholas Ramkissoon <nicholas.ramkissoon01@gmail.com>
2022-04-07 12:44:40 -0400

    searchable blog posts + tags

--- application/webapp/src/modules/blog/components/Index-Page.tsx
+++ application/webapp/src/modules/blog/components/Index-Page.tsx
@@ -1,9 +1,47 @@
-import { Badge, Box, chakra, Flex } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+import {
+  Badge,
+  Box,
+  chakra,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Fuse from "fuse.js";
+import { uniqueId } from "lodash";
 import NextLink from "next/link";
+import React from "react";
 import { FrontMatter } from "../utils";
+
+const getTagColor = (tag: string) => {
+  switch (tag) {
+    case "aws":
+      return { bg: "#FF9900", color: "white" };
+    case "monitoring":
+      return { bg: "blue.400", color: "white" };
+    case "saas":
+      return { bg: "purple.400", color: "white" };
+    case "tutorial":
+      return { bg: "gray.400", color: "white" };
+    default:
+      return { bg: "gray.500", color: "white" };
+  }
+};
+
+const Tag: React.FC<{ tag: string }> = ({ tag }) => {
+  const colors = getTagColor(tag);
+  return (
+    <Badge color={colors.color} bg={colors.bg} px="6px">
+      {tag}
+    </Badge>
+  );
+};
 
 function BlogPostPanel(props: { frontMatter: FrontMatter }) {
   const { frontMatter } = props;
+
   return (
     <Flex
       bg={"white"}
@@ -22,6 +60,13 @@ function BlogPostPanel(props: { frontMatter: FrontMatter }) {
     >
       <NextLink href={frontMatter.slug as string} passHref>
         <Box>
+          {frontMatter.tags && frontMatter.tags?.length > 0 && (
+            <Flex gap={2}>
+              {frontMatter.tags.map((t) => (
+                <Tag key={uniqueId()} tag={t} />
+              ))}
+            </Flex>
+          )}
           <chakra.h2 fontSize="3xl" fontWeight="bold">
             {frontMatter.title}
           </chakra.h2>
@@ -40,6 +85,7 @@ function BlogPostPanel(props: { frontMatter: FrontMatter }) {
           <chakra.p mb=".8em" fontSize="lg">
             {frontMatter.description}
           </chakra.p>
+
           <chakra.p
             fontSize="lg"
             fontWeight="medium"
@@ -59,11 +105,17 @@ function BlogPostPanel(props: { frontMatter: FrontMatter }) {
 export function IndexPage(props: { frontMatters: FrontMatter[] }) {
   const { frontMatters } = props;
 
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const fuse = new Fuse(frontMatters, {
+    keys: ["title", "tags", "description"],
+  });
+  const results = fuse.search(searchQuery);
+
   const Header = (
     <Flex
       justifyContent="center"
       flexDir="column"
-      mb="4em"
+      mb="2em"
       w="full"
       mt="3em"
       px="2"
@@ -85,35 +137,55 @@ export function IndexPage(props: { frontMatters: FrontMatter[] }) {
         maxW={"2xl"}
         lineHeight="shorter"
       >
-        Learn about monitoring, DevOps, and running an reliable online business.
+        Learn about monitoring, DevOps, and running online businesses.
       </chakra.span>
     </Flex>
   );
 
-  if (!frontMatters || frontMatters.length === 0) {
-    return (
-      <Box flexGrow="1" className="bg-blog-tile" bgRepeat="repeat">
-        {Header}
-        <chakra.h1 fontSize="xl" fontWeight="bold" textAlign="center">
-          Looks like there's nothing here yet....
-        </chakra.h1>
-      </Box>
-    );
-  }
   return (
     <Box flexGrow="1" className="bg-blog-tile" bgRepeat="repeat">
       {Header}
-      <Flex alignItems="center" flexDir="column" px="2">
-        {frontMatters
-          .sort((a, b) =>
-            new Date(a.lastEdited?.date ?? 0).getTime() <
-            new Date(b.lastEdited?.date ?? 0).getTime()
-              ? 1
-              : -1
-          )
-          .map((frontMatter: FrontMatter) => (
-            <BlogPostPanel key={frontMatter.slug} frontMatter={frontMatter} />
-          ))}
+      <InputGroup
+        mr="1em"
+        mb="30px"
+        w="2xl"
+        mx="auto"
+        border="1px"
+        borderColor="gray.300"
+        borderRadius="lg"
+        shadow="sm"
+      >
+        <InputLeftElement
+          pointerEvents="none"
+          children={<SearchIcon color="gray.300" />}
+        />
+        <Input
+          shadow="sm"
+          placeholder="Search posts..."
+          background={useColorModeValue("white", "gray.950")}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </InputGroup>
+      <Flex alignItems="center" flexDir="column" px={5}>
+        {searchQuery === ""
+          ? frontMatters
+              .sort((a: any, b: any) =>
+                new Date(a.lastEdited?.date ?? 0).getTime() <
+                new Date(b.lastEdited?.date ?? 0).getTime()
+                  ? 1
+                  : -1
+              )
+              .slice(0, 5)
+              .map((frontMatter: FrontMatter) => (
+                <BlogPostPanel
+                  key={frontMatter.slug}
+                  frontMatter={frontMatter}
+                />
+              ))
+          : results.map((r) => (
+              <BlogPostPanel key={r.item.slug} frontMatter={r.item} />
+            ))}
       </Flex>
     </Box>
   );
